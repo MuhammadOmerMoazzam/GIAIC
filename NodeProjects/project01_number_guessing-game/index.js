847d813fd53dbac977a8016703034e30d779f5b4
MuhammadOmerMoazzam <153811108+MuhammadOmerMoazzam@users.noreply.github.com>
2024-03-23 20:31:10 +0500

    Updated index.js

---
+++ NodeProjects/project01_number_guessing-game/index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// generating random number
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+// // taking user input
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Guess a Number between 1 to 10: "
+    },
+]);
+// applying conditional statement
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(`Congratulations! you guessed the right number.`);
+}
+else {
+    console.log(`Sorry! you guessed wrong number. The number was ${randomNumber}. Lets try again.`);
+}
