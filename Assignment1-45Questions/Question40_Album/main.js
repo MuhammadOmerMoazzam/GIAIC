cc2111dd1754fc5e438b58172766edf32726dbe2
MuhammadOmerMoazzam <153811108+MuhammadOmerMoazzam@users.noreply.github.com>
2024-05-12 03:53:31 +0500

    Added Question40

---
+++ Assignment1-45Questions/Question40_Album/main.js
@@ -0,0 +1,24 @@
+"use strict";
+const make_album = (artistName, albumTitle, numOfTracks = 0) => {
+    if (numOfTracks !== 0) {
+        let album = {
+            "Artist Name": artistName,
+            "Album Title": albumTitle,
+            "Number of Tracks": numOfTracks,
+        };
+        return album;
+    }
+    else {
+        let album = {
+            "Artist Name": artistName,
+            "Album Title": albumTitle,
+        };
+        return album;
+    }
+};
+// calling the function three times with different parameters
+console.log(make_album("artist1", "title1"));
+console.log(make_album("artist2", "title2"));
+console.log(make_album("artist3", "title3"));
+// one time with number of tracks defined
+console.log(make_album("artist4", "title4", 8));
