f03b971b99e29142fffc2b204620f69e80fb6e29
MuhammadOmerMoazzam <153811108+MuhammadOmerMoazzam@users.noreply.github.com>
2024-05-12 01:12:41 +0500

    Added Question38

---
+++ Assignment1-45Questions/Question38_Cities/main.js
@@ -0,0 +1,9 @@
+"use strict";
+const city_country = (city, country = "Pakistan") => {
+    console.log(`${city} is in ${country}.`);
+};
+// calling unction for three different cities, 
+// at least one of which is not in the default country
+city_country("Karachi");
+city_country("Lahore");
+city_country("London", "England");
