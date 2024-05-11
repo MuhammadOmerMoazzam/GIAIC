63ce9da81f8f9bf4a84cdcc4639ac926e4c5a89e
MuhammadOmerMoazzam <153811108+MuhammadOmerMoazzam@users.noreply.github.com>
2024-05-12 04:54:27 +0500

    Added Question22

---
+++ Assignment1-45Questions/Question22_IntentionalError/main.ts
@@ -0,0 +1,28 @@
+// Original array
+let programs: string[] = [
+    "Billing Software",
+    "Password Generator",
+    "GUI Calculator",
+    "ToDoList",
+    "Online Quiz System",
+];
+
+// display original array
+console.log("\nOriginal array = ", programs);
+
+// intentional error: Accessing an index that doesn't exist
+let index = 10;
+
+// In TypeScript (and JavaScript), 
+// accessing an out-of-bounds index simply returns undefined without throwing an error. 
+console.log("\nprograms[10] = ", programs[index]) // undefined
+
+// correcting the error by checking the array length
+if (index >= 0 && index < programs.length) {
+
+  console.log("\nValue at index", index, ":", programs[index]);
+
+} else {
+
+  console.error("\nIndex error: Index out of bounds");
+}
