fe26cb15bc4fc3102b36c4e5fd29c4a3fb2b5aef
MuhammadOmerMoazzam <153811108+MuhammadOmerMoazzam@users.noreply.github.com>
2024-10-13 23:45:44 +0500

    worked on responsiveness and styling.

--- NextJS/assignments/assignment2-portolio-using-html-css/script.js
+++ NextJS/assignments/assignment2-portolio-using-html-css/script.js
@@ -1,20 +1,18 @@
-const hamburger = document.querySelector(".hamburger") as HTMLElement;
-const navMenu = document.querySelector("nav ul") as HTMLUListElement;
-
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav ul");
 hamburger.addEventListener("click", toggleMenu);
-function toggleMenu(): void {
+function toggleMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
 document.querySelectorAll("nav a").forEach(n => n.addEventListener("click", closeMenu));
-
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
-
-const text = document.querySelector('.horizontal-text') as HTMLElement;
+const text = document.querySelector('.horizontal-text');
 window.addEventListener('scroll', () => {
-    const scrollPos: number = window.scrollY;
+    const scrollPos = window.scrollY;
     text.style.transform = `translateX(${-scrollPos}px)`;
 });
+export {};
