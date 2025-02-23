c5755aa0f3cfe37392d5bc78d1f922887bf87219
MuhammadOmerMoazzam <153811108+MuhammadOmerMoazzam@users.noreply.github.com>
2025-02-24 01:58:11 +0500

    created a chatbot and navigation to access different pages

---
+++ Streamlit/Growth_Mindset_Challenge/chatbot.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from openai import OpenAI
+import os
+
+st.title("🤖 Chatbot")
+if "toast_shown" not in st.session_state:
+  st.toast("""We use the **Deepseek-R1 Reasoning model** for advanced, thoughtful outputs. Enjoy the conversation! ✨""", icon="🤗")
+  st.session_state.toast_shown = True
+  
+  
+# setting openrouter api key and default model
+client = OpenAI(
+  api_key=os.getenv("OPENROUTER_API_KEY"),
+  base_url="https://openrouter.ai/api/v1"
+)
+
+if "deepseek_model" not in st.session_state:
+  st.session_state["deepseek_model"] = "deepseek/deepseek-r1:free"
+
+# managing chat history
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+for msg in st.session_state.messages:
+  st.chat_message(msg["role"]).markdown(msg["content"])
+
+# handling user input and openai response
+promt = st.chat_input("How may I assist you?")
+if promt:
+  st.chat_message("user").markdown(promt)
+  st.session_state.messages.append({
+    "role":"user", "content": promt
+  })
+
+  with st.chat_message("assistant"):
+    try:
+      stream = client.chat.completions.create(
+        model = st.session_state["deepseek_model"],
+        messages=[
+          {"role": msg["role"], "content":msg["content"]}
+          for msg in st.session_state.messages
+        ],
+        stream=True
+    )
+    except Exception as e: 
+      st.error(e.message) 
+    else:
+      response = st.write_stream(stream)
+      st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ Streamlit/Growth_Mindset_Challenge/home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.logo(
+    image="images/logo.svg",
+    icon_image="images/logo.svg",
+  )
+
+def home():
+  st.balloons()
+  st.title("All-in-One AI Assistant")
+  st.write("Hello World")
+
+
+pg = st.navigation([
+  st.Page(home, title="Home"), 
+  st.Page("chatbot.py", title="Chatbot"),
+  st.Page("data_visualization.py", title="Data Visualization")
+])
+pg.run()
+
+
